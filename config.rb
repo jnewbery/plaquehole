473169218c89a28e71343d5c8cbcbe0b98421105
jonnynewbs <jonnynewbs@gmail.com>
2015-07-11 19:20:23 +1000

    octopress checkins

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/plaquehole/"
+http_images_path = "/plaquehole/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/plaquehole/fonts"
+css_dir = "public/plaquehole/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
